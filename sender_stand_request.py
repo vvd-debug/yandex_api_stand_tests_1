b96085e21ca0f68812355f720346c7bb6b965add
Your Name <you@example.com>
2025-01-11 18:28:22 +0300

    Обновлен файл с тестами

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,45 +1,142 @@
-import config
-import requests
+import sender_stand_request
 import data
 
-def get_logs():
-    # Складываем базовый URL из конфигурационного файла и путь к основным логам,
-    # чтобы сформировать полный URL для запроса.
-    # Возвращает объект ответа, полученный от сервера после выполнения GET-запроса
-    return requests.get(config.URL_SERVICE + config.LOG_MAIN_PATH,
-                        params={"count": 20})
 
-# Функция для получения данных из таблицы пользователей
-def get_users_table():
-    # Составление полного URL пути к данным таблицы пользователей
-    # путем конкатенации базового URL сервиса и конечной точки таблицы пользователей
-    # Возвращает объект ответа от сервера
-    return requests.get(config.URL_SERVICE + config.USERS_TABLE_PATH)
+# Функция для изменения значения в параметре firstName в теле запроса
+def get_user_body(first_name):
+    # Копируется словарь с телом запроса из файла data
+    current_body = data.user_body.copy()
+    # Изменение значения в поле firstName
+    current_body["firstName"] = first_name
+    # Возвращается новый словарь с нужным значением firstName
+    return current_body
 
 
-# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+# Функция для позитивной проверки
+def positive_assert(first_name):
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body(first_name)
+    # В переменную user_response сохраняется результат запроса на создание пользователя:
+    user_response = sender_stand_request.post_new_user(user_body)
 
-def post_new_user(body):
-    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
-    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
-    # json=body используется для отправки данных пользователя в формате JSON
-    # headers=data.headers устанавливает заголовки запроса из модуля data
-    return requests.post(config.URL_SERVICE + config.CREATE_USER_PATH,
-                         json=body,
-                         headers=data.headers)
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201
+    # Проверяется, что в ответе есть поле authToken и оно не пустое
+    assert user_response.json()["authToken"] != ""
 
-def post_products_kits(products_ids):
-    # Отправка POST-запроса с использованием URL из конфигурации, данных о продуктах и заголовков
-    # Возвращается объект ответа, полученный от сервера
-    return requests.post(config.URL_SERVICE + config.PRODUCTS_KITS_PATH,
-                         json=products_ids,  # Тело запроса содержит ID продуктов в формате JSON
-                         headers=data.headers)  # Использование заголовков из файла data.py
+    # В переменную users_table_response сохраняется результат запроса
+    users_table_response = sender_stand_request.get_users_table()
+
+    # Строка, которая должна быть в ответе
+    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
+               + user_body["address"] + ",,," + user_response.json()["authToken"]
+
+    # Проверка, что такой пользователь есть и он единственный
+    assert users_table_response.text.count(str_user) == 1
 
 
-# Вызов функции post_new_user с телом запроса для создания нового пользователя из модуля data
-#response = post_new_user(data.user_body);
+# Функция негативной проверки, когда в ответе ошибка про символы
+def negative_assert_symbol(first_name):
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body(first_name)
 
-# Вывод HTTP-статус кода ответа на запрос
-# Код состояния указывает на результат обработки запроса сервером
-#print(response.status_code)
-#print(response.json())
+    # В переменную response сохраняется результат
+    response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 400
+    assert response.status_code == 400
+
+    # Проверяется, что в теле ответа атрибут "code" равен 400
+    assert response.json()["code"] == 400
+    # Проверяется текст в теле ответа в атрибуте "message"
+    assert response.json()["message"] == "Имя пользователя введено некорректно. " \
+                                         "Имя может содержать только русские или латинские буквы, " \
+                                         "Длина должна быть не менее 2 и не более 15 символов"
+
+
+# Функция для негативной проверки, когда в ответе ошибка: "Не все необходимые параметры были переданы"
+def negative_assert_no_firstname(user_body):
+    # В переменную response сохраняется результат
+    response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 400
+    assert response.status_code == 400
+
+    # Проверяется, что в теле ответа атрибут "code" равен 400
+    assert response.json()["code"] == 400
+    # Проверяется текст в теле ответа в атрибуте "message"
+    assert response.json()["message"] == "Не все необходимые параметры были переданы"
+
+
+# Тест 1. Успешное создание пользователя. Параметр firstName состоит из 2 символов
+def test_create_user_2_letter_in_first_name_get_success_response():
+    positive_assert("Aa")
+
+
+# Тест 2. Успешное создание пользователя. Параметр firstName состоит из 15 символов
+def test_create_user_15_letter_in_first_name_get_success_response():
+    positive_assert("Ааааааааааааааа")
+
+
+# Тест 3. Ошибка. Параметр firstName состоит из 1 символа
+def test_create_user_1_letter_in_first_name_get_error_response():
+    negative_assert_symbol("A")
+
+
+# Тест 4. Ошибка. Параметр firstName состоит из 16 символов
+def test_create_user_16_letter_in_first_name_get_error_response():
+    negative_assert_symbol("Аааааааааааааааa")
+
+
+# Тест 5. Успешное создание пользователя. Параметр fisrsName состоит из английских букв
+def test_create_user_english_letter_in_first_name_get_success_response():
+    positive_assert("QWErty")
+
+
+# Тест 6. Успешное создание пользователя. Параметр firstName состоит из русских букв
+def test_create_user_russian_letter_in_first_name_get_success_response():
+    positive_assert("Мария")
+
+
+# Тест 7. Ошибка. Параметр firstName состоит из слов с пробелами
+def test_create_user_has_space_in_first_name_get_error_response():
+    negative_assert_symbol("Человек и КО")
+
+
+# Тест 8. Ошибка. Параметр firstName состоит из строки спецсимволов
+def test_create_user_has_special_symbol_in_first_name_get_error_response():
+    negative_assert_symbol("\"№%@\",")
+
+
+# Тест 9. Ошибка. Параметр firstName состоит из строки цифр
+def test_create_user_has_number_in_first_name_get_error_response():
+    negative_assert_symbol("123")
+
+
+# Тест 10. Ошибка. В запросе нет параметра firstName
+def test_create_user_no_first_name_get_error_response():
+    # Копируется словарь с телом запроса из файла data в переменную user_body
+    user_body = data.user_body.copy()
+    # Удаление параметра firstName из запроса
+    user_body.pop("firstName")
+    # Проверка полученного ответа
+    negative_assert_no_firstname(user_body)
+
+
+# Тест 11. Ошибка. Параметр состоит из пустой строки
+def test_create_user_empty_first_name_get_error_response():
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body("")
+    # Проверка полученного ответа
+    negative_assert_no_firstname(user_body)
+
+
+# Тест 12. Ошибка. Тип параметра firstName: число
+def test_create_user_number_type_first_name_get_error_response():
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body(12)
+    # В переменную user_response сохраняется результат запроса на создание пользователя:
+    response = sender_stand_request.post_new_user(user_body)
+
+    # Проверка кода ответа
+    assert response.status_code == 400
